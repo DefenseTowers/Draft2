d1f689dd9b55a4c749881d8231941e91d975b22f
OneTrueBears <bovstedal95@gmail.com>
2018-04-16 11:42:32 +0200

    Updated maptile, added numerous sprites
    Added: Gold Display, updated play and settings button. Added tile-files
    for map.

---
+++ android/assets/greytilemap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="GreyTilemap" tilewidth="20" tileheight="20" tilecount="1408" columns="44">
+ <image source="../../../../Dropbox/Photoshop/Clash of Defense Towers/maptile.png" width="886" height="648"/>
+</tileset>
---
+++ android/assets/shoptile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="shop" tilewidth="20" tileheight="20" tilecount="544" columns="17">
+ <image source="../../../../Dropbox/Photoshop/Clash of Defense Towers/shoptile.png" width="350" height="648"/>
+</tileset>
